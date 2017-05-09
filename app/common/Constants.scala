428f325b0b77ce58a62f387a8539e7d5aad804af
liboren <liboren920528@sina.com>
2017-05-09 12:17:01 +0800

    update

--- app/common/Constants.scala
+++ app/common/Constants.scala
@@ -18,10 +18,14 @@ object Constants {
     val VAGUE_MATCH = 0 // 关键词模糊匹配
   }
   object FilePath {
-    val IMG_PATH = "C:\\Users\\Macbook\\Desktop\\img\\" //图片存储路径
-    val EMOTION_PATH = "C:\\Users\\Macbook\\Desktop\\emotion\\" //表情存储路径
-    val VIDEO_PATH = "C:\\Users\\Macbook\\Desktop\\video\\" //视频存储路径
-    val AUDIO_PATH = "C:\\Users\\Macbook\\Desktop\\voice\\" //音频存储路径
+    val IMG_PATH = "/usr/tmp/img/" //图片存储路径
+    val EMOTION_PATH = "/usr/tmp/emotion/" //表情存储路径
+    val VIDEO_PATH = "/usr/tmp/video/" //视频存储路径
+    val AUDIO_PATH = "/usr/tmp/voice/" //音频存储路径
+//    val IMG_PATH = "C:\\Users\\Macbook\\Desktop\\img\\" //图片存储路径
+//    val EMOTION_PATH = "C:\\Users\\Macbook\\Desktop\\emotion\\" //表情存储路径
+//    val VIDEO_PATH = "C:\\Users\\Macbook\\Desktop\\video\\" //视频存储路径
+//    val AUDIO_PATH = "C:\\Users\\Macbook\\Desktop\\voice\\" //音频存储路径
   }
   object FileType{
     val IMG_TYPE = "jpeg"
